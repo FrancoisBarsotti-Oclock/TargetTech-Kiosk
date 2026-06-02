09ef93325277a32468d0aec474e13939fb611d1f
unknown <francois.barsotti@oclock.school>
2026-06-02 16:17:46 +0200

    feat: Modif script 17 pour corriger format de sous-domaines gestion micro/caméra

--- Scripts/17-Configure-ChromeHardeningFull.ps1
+++ Scripts/17-Configure-ChromeHardeningFull.ps1
@@ -74,9 +74,11 @@ foreach ($Url in $AllowUrls) {
 # ------------------------------------------------------------
 
 New-Item -Path "$ChromePolicyRoot\AudioCaptureAllowedUrls" -Force | Out-Null
-New-ItemProperty -Path "$ChromePolicyRoot\AudioCaptureAllowedUrls" -Name "1" -Value "https://target-tech.fr/*" -PropertyType String -Force | Out-Null
+New-ItemProperty -Path "$ChromePolicyRoot\AudioCaptureAllowedUrls" -Name "1" -Value "target-tech.fr" -PropertyType String -Force | Out-Null
+New-ItemProperty -Path "$ChromePolicyRoot\AudioCaptureAllowedUrls" -Name "2" -Value "peersjs.target-tech.fr" -PropertyType String -Force | Out-Null
 
 New-Item -Path "$ChromePolicyRoot\VideoCaptureAllowedUrls" -Force | Out-Null
-New-ItemProperty -Path "$ChromePolicyRoot\VideoCaptureAllowedUrls" -Name "1" -Value "https://target-tech.fr/*" -PropertyType String -Force | Out-Null
+New-ItemProperty -Path "$ChromePolicyRoot\VideoCaptureAllowedUrls" -Name "1" -Value "target-tech.fr" -PropertyType String -Force | Out-Null
+New-ItemProperty -Path "$ChromePolicyRoot\VideoCaptureAllowedUrls" -Name "2" -Value "peersjs.target-tech.fr" -PropertyType String -Force | Out-Null
 
 Write-Log "Chrome Hardening complet appliqué."
